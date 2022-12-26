3b088128ecb20fa6df31c3d2f00eb1f41829f62e
yjq <yjq@wust.com>
2022-12-26 14:55:35 +0800

    hot-fix first commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello,git!");
         System.out.println("hello222,git!");
         System.out.println("hello333,git!");
+        System.out.println("hot-fix");
     }
 }
